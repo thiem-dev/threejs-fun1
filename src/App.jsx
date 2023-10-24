3da9e8eb656dd86cae336e5417c013bcde4126cd
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-24 02:02:30 -0400

    camera orbit controls added

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,21 @@ function App() {
   //loads canvas renderer when canvas element loads
   useEffect(() => {
 
-
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector("#threejs-canvas")
     });
+
+
     renderer.setSize( window.innerWidth/2, window.innerHeight/2)
     camera.aspect = window.innerWidth / window.innerHeight;
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     // document.body.appendChild( renderer.domElement );
 
+
+    // make box
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material );
@@ -38,6 +41,8 @@ function App() {
       requestAnimationFrame( animate );
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
+
+      controls.update();
 
       renderer.render( scene, camera );
     }
